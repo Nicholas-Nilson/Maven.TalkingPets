3f8c862de47506776e47084c3fdde98ae930ba13
Nick Nilson <nilson.nicholas@gmail.com>
2022-06-30 18:14:55 -0400

    first update, Pet class & test class made

--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -1,4 +1,13 @@
 package io.zipcoder.polymorphism;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class MainApplication {
+    Scanner interviewer = new Scanner(System.in);
+    Integer numberOfPets = 0;
+    ArrayList<Integer> typeOfPet = new ArrayList<Integer>();
+    ArrayList<String> nameOfPet = new ArrayList<String>();
+
+
 }
