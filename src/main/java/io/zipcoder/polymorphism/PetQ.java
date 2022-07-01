00d32b73e725579eca8b7e0bc55e04f5e082de71
Nick Nilson <nilson.nicholas@gmail.com>
2022-07-01 09:24:08 -0400

    updated input check for # of pets

--- src/main/java/io/zipcoder/polymorphism/PetQ.java
+++ src/main/java/io/zipcoder/polymorphism/PetQ.java
@@ -14,30 +14,40 @@ public class PetQ {
     public static ArrayList<String> nameOfPet = new ArrayList<String>();
 
     public static void main(String[] args) {
+    boolean petNum = true;
+        //Number of Pets
         System.out.println("How many pets do you have?");
-        while (numberOfPets == -1) {
-            try {
-                numberOfPets = interviewer.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("Please enter a number.");
-                numberOfPets = -1;
-                interviewer.nextLine();
-            }
-        }
-        numberOfPets = interviewer.nextInt();
-        while (numberOfPets < 0) {
-            System.out.println("We consider pets positive around here. Please enter a non-negative number.");
-            numberOfPets = interviewer.nextInt();
-        }
+         while (petNum) {
+             try {
+                 numberOfPets = interviewer.nextInt();
+             } catch (InputMismatchException e) {
+                 System.out.println("Please enter a number.");
+//                 numberOfPets = -1;
+                 interviewer.next();
+                 continue;
+             }
+             if (numberOfPets < 0) {
+                 System.out.println("Please enter a non-negative number.");
+                 continue;
+             }
+             petNum = false;
+         }
+        //Asking User to Specify Animal Types
         if (numberOfPets > 0){
         for (int i = 1; i <= numberOfPets; i++) {
-            System.out.printf("What is the name of pet #%d?\n", i);
+            System.out.printf("What kind of animal is pet #%d?\n", i);
             System.out.println("Please Enter:");
-            System.out.println("'1' if it's a Dog");
-            System.out.println("'2' if it's Cat");
-            System.out.println("'3' if it's Duck");
+            System.out.println("'1' for Dog");
+            System.out.println("'2' for Cat");
+            System.out.println("'3' for Duck");
             typeOfPet.add(interviewer.nextInt());
         }
+        }
+
+        //Asking User for Names of Pets
+        for (int pet : typeOfPet) {
+            System.out.printf("What is pet #%d's name?\n", pet);
+            nameOfPet.add(interviewer.next());
         }
     }
     }
