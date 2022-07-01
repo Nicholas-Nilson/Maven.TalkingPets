95b7259e091e91e11b10eae600c9dba7077ea5bd
Nick Nilson <nilson.nicholas@gmail.com>
2022-07-01 13:56:12 -0400

    type of pet loop fixed, still not in a method -_-

--- src/main/java/io/zipcoder/polymorphism/PetQ.java
+++ src/main/java/io/zipcoder/polymorphism/PetQ.java
@@ -1,5 +1,7 @@
 package io.zipcoder.polymorphism;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
@@ -12,48 +14,104 @@ public class PetQ {
     public static Integer numberOfPets = -1;
     public static ArrayList<Integer> typeOfPet = new ArrayList<Integer>();
     public static ArrayList<String> nameOfPet = new ArrayList<String>();
+    public static boolean gotPetNumber = false;
+    public static boolean petQComplete = false;
+    private static String[] instructions = {"How many pets do you have?", "Please select type of pet", "What are their names?"};
+
+//    public static void inputCheck(int varToUpdate, String instruct) {
+//        boolean notNumber = true;
+//        while (notNumber) {
+//            System.out.println(instruct);
+//            try {
+//                varToUpdate = interviewer.nextInt();
+//                notNumber = false;
+//            } catch (InputMismatchException e) {
+//                System.out.println("Please enter a number.");
+//                interviewer.next();
+//                continue;
+//            }
+//            if (varToUpdate < 0) {
+//                System.out.println("Please enter a non-negative number.");
+//                continue;
+//            }
+//            notNumber = false;
+//        }
+//    }
 
     public static void main(String[] args) {
-    boolean petNum = true;
+
+
         //Number of Pets
-        System.out.println("How many pets do you have?");
-         while (petNum) {
+         boolean gotPetNumber = false;
+         while (!gotPetNumber) {
+             System.out.println("How many pets do you have?");
              try {
                  numberOfPets = interviewer.nextInt();
              } catch (InputMismatchException e) {
                  System.out.println("Please enter a number.");
-//                 numberOfPets = -1;
                  interviewer.next();
                  continue;
              }
              if (numberOfPets < 0) {
                  System.out.println("Please enter a non-negative number.");
                  continue;
-             }
-             petNum = false;
+             }  //needs a way to end the program if the user enters 0 pets.
+             gotPetNumber = true;
          }
+
         //Asking User to Specify Animal Types
-        if (numberOfPets > 0){
-        for (int i = 1; i <= numberOfPets; i++) {
-            System.out.printf("What kind of animal is pet #%d?\n", i);
-            System.out.println("Please Enter:");
-            System.out.println("'1' for Dog");
-            System.out.println("'2' for Cat");
-            System.out.println("'3' for Duck");
-            typeOfPet.add(interviewer.nextInt());
-        }
-        }
+        // could use way to verify an input of 1, 2, or 3 was selected -_-.
+        // the method of removing indexes isn't working. may be better to use an array set to size numOfPets.
+     if (numberOfPets > 0) {
+//         Integer[] typeOfPet = new Integer[numberOfPets]; //if changing code to methods, figure out how to define this.
+         for (int i = 1; i <= numberOfPets; i++) {
+             boolean gotPetType = false;
+             int petType;
+             while (!gotPetType) {
+                 System.out.printf("What kind of animal is pet #%d?\n", i);
+                System.out.println("Please Enter:");
+                System.out.println("'1' for Dog");
+                System.out.println("'2' for Cat");
+                System.out.println("'3' for Duck");
+                try {
+                    petType = interviewer.nextInt();
+                } catch (InputMismatchException e) {
+                    System.out.println("Please enter a number.");
+                    interviewer.next();
+                    continue;
+                } if (petType <= 0 || petType > 3) {
+                     System.out.println("Please enter a correct option.");
+                     continue;
+                 }
+                gotPetType = true;
+                typeOfPet.add(petType);
+                System.out.println(i);
+                }
+         }
+     }
+
+//        if (numberOfPets > 0) {   //before adding value check for types of animals available in the program.
+//            for (int i = 1; i <= numberOfPets; i++) {
+//                System.out.printf("What kind of animal is pet #%d?\n", i);
+//                System.out.println("Please Enter:");
+//                System.out.println("'1' for Dog");
+//                System.out.println("'2' for Cat");
+//                System.out.println("'3' for Duck");
+//                typeOfPet.add(interviewer.nextInt());
+//            }
+//        }
 
         //Asking User for Names of Pets. Can also create the pets here.
 
         int dogCount = 0;
         int catCount = 0;
-        int duckCount =0;
+        int duckCount = 0;
         for (int pet : typeOfPet) {
             System.out.printf("What is pet #%d's name?\n", pet);
             nameOfPet.add(interviewer.next());
         }
         System.out.println(typeOfPet);
         System.out.println(nameOfPet);
+        petQComplete = true;
     }
-    }
+}
