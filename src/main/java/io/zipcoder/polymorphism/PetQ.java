e8359b012faef2fb1c1c40305a66907a6dbeb2b4
Nick Nilson <nilson.nicholas@gmail.com>
2022-07-01 00:29:55 -0400

    fixed inputmismatch possibility on getting # of pets

--- src/main/java/io/zipcoder/polymorphism/PetQ.java
+++ src/main/java/io/zipcoder/polymorphism/PetQ.java
@@ -2,21 +2,31 @@ package io.zipcoder.polymorphism;
 
 import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class PetQ {
 
     //Should I have a method to fill in the variables? Or ... idk.. we're just trying to get the information here!
     private static Scanner interviewer = new Scanner(System.in);
-    public static Integer numberOfPets;
+    public static Integer numberOfPets = -1;
     public static ArrayList<Integer> typeOfPet = new ArrayList<Integer>();
     public static ArrayList<String> nameOfPet = new ArrayList<String>();
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("How many pets do you have?");
+        while (numberOfPets == -1) {
+            try {
+                numberOfPets = interviewer.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a number.");
+                numberOfPets = -1;
+                interviewer.nextLine();
+            }
+        }
         numberOfPets = interviewer.nextInt();
         while (numberOfPets < 0) {
-            System.out.println("We consider pets positive around here. It's ok if you have zero, if not, tell us how many you have.");
+            System.out.println("We consider pets positive around here. Please enter a non-negative number.");
             numberOfPets = interviewer.nextInt();
         }
         if (numberOfPets > 0){
